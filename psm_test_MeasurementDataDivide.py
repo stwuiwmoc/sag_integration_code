799035fbb15da1f301148b1841e604590f5891bb
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-01-12 22:28:05 +0900

    モジュール変更が反映されるようにimportlibを追加

--- psm_test_MeasurementDataDivide.py
+++ psm_test_MeasurementDataDivide.py
@@ -2,8 +2,11 @@
 import planets_sag_myclass as psm
 import numpy as np
 import matplotlib.pyplot as plt
+import importlib
 
 if __name__=="__main__":
+    importlib.reload(psm)
+    
     CONSTS = psm.Constants(pitch_length=20e-3)
     measurement = psm.MeasurementDataDivide(filepath="raw_data/1216/RT_202112161123_FTP.txt")
     
