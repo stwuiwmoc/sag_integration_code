eb857b1d9864c02ff6ebfc5bb81c10bc93d5d2c3
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-01-13 09:17:21 +0900

    pep8でオートフォーマット

--- planets_sag_myclass.py
+++ planets_sag_myclass.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 from typing import List
 import numpy as np
 import scipy as sp
@@ -7,15 +7,19 @@ import pickle
 import csv
 import pandas as pd
 
+
 def mkhelp(instance):
     import inspect
     attr_list = list(instance.__dict__.keys())
     for attr in attr_list:
-        if attr.startswith("_"): continue
+        if attr.startswith("_"):
+            continue
         print(attr)
     for method in inspect.getmembers(instance, inspect.ismethod):
-        if method[0].startswith("_"): continue
-        print(method[0]+"()")
+        if method[0].startswith("_"):
+            continue
+        print(method[0] + "()")
+
 
 class Constants:
     def __init__(self, pitch_length) -> None:
@@ -27,13 +31,14 @@ class Constants:
         pitch_length : float
             picth length in iterated integral（逐次積分）
         """
-        self.pitch_length=pitch_length
-        
+        self.pitch_length = pitch_length
+
     def h(self):
         mkhelp(self)
 
+
 class MeasurementDataDivide:
-    def __init__(self, filepath:str, skiprows:int=3) -> None:
+    def __init__(self, filepath: str, skiprows: int = 3) -> None:
         """class : MeasurementDataDivede
 
         Parameters
@@ -44,29 +49,33 @@ class MeasurementDataDivide:
             skip row number in pd.read_csv()
             by default 3
         """
-        self.filepath=filepath
-        self.raw=pd.read_csv(self.filepath, skiprows=skiprows, delimiter=" ")
-        
-        self.raw_df_list=self.__raw_data_divide()
-        
+        self.filepath = filepath
+        self.raw = pd.read_csv(self.filepath, skiprows=skiprows, delimiter=" ")
+
+        self.raw_df_list = self.__raw_data_divide()
+
         return
-    
+
     def h(self):
         mkhelp(self)
-    
+
     def __raw_data_divide(self) -> List:
         df_raw = self.raw
         df_list = []
-        j=0
+        j = 0
         for i in range(len(df_raw)):
-            if df_raw.iloc[i:i+1]["Idx."].values < df_raw.iloc[i+1:i+2]["Idx."].values:
+            if df_raw.iloc[i:i +
+                           1]["Idx."].values < df_raw.iloc[i +
+                                                           1:i +
+                                                           2]["Idx."].values:
                 pass
             else:
                 print(j)
-                df_temp = df_raw.iloc[j+1:i+1]
-                df_temp["sag"] =( 2* df_temp["Out2"] - (df_temp["Out1"] +df_temp["Out3"]))/2
-                
+                df_temp = df_raw.iloc[j + 1:i + 1]
+                df_temp["sag"] = (2 * df_temp["Out2"] -
+                                  (df_temp["Out1"] + df_temp["Out3"])) / 2
+
                 df_list.append(df_temp)
                 j = i
-                    
+
         return df_list
