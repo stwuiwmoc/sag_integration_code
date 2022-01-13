df0b24a1e7ddd803081dca746a4df828ce111623
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-01-13 11:04:22 +0900

    測定序盤と終盤のthetaの重複を削除する関数remove_theta_duplicationを作成

--- planets_sag_myclass.py
+++ planets_sag_myclass.py
@@ -1,6 +1,7 @@
 # %%
 from typing import List
 import numpy as np
+from pandas.core.frame import DataFrame
 import scipy as sp
 import matplotlib.pyplot as plt
 import pickle
@@ -33,7 +34,7 @@ class Constants:
         """
         self.pitch_length = pitch_length
 
-    def h(self):
+    def h(self) -> None:
         mkhelp(self)
 
 
@@ -56,7 +57,7 @@ class MeasurementDataDivide:
 
         return
 
-    def h(self):
+    def h(self) -> None:
         mkhelp(self)
 
     def __raw_data_divide(self) -> List:
@@ -75,3 +76,48 @@ class MeasurementDataDivide:
                 j = i
 
         return df_list
+
+
+class CirclePathIntegration:
+    def __init__(self, DataFrame: DataFrame) -> None:
+        self.df_raw = DataFrame
+        self.df = self.__remove_theta_duplication(theta_end_specifying_value=-19)
+        return
+
+    def h(self) -> None:
+        mkhelp(self)
+
+    def __remove_theta_duplication(self, theta_end_specifying_value: float) -> DataFrame:
+        """測定序盤と終盤のthetaの重複分を除去
+
+        Parameters
+        ----------
+        theta_end_specifying_value : float
+            終盤で同じthetaが出力される部分があるので、どのthetaまでで切るか
+            -15~-19くらいが良いと思われる
+
+        Returns
+        -------
+        df_remove_duplication : DataFrame
+            df_rawからthetaの重複部分を除去したもの
+        """
+        df_theta = self.df_raw["theta"]
+
+        for i in range(len(df_theta)):
+            j = -(i + 1)
+            if df_theta.iloc[j] < theta_end_specifying_value:
+                pass
+            else:
+                end_duplicate_last_idx = j + 1
+                theta_end = df_theta.iloc[end_duplicate_last_idx]
+                break
+
+        for i in range(len(df_theta)):
+            if df_theta.iloc[i] > theta_end:
+                pass
+            else:
+                head_duplicate_last_idx = i - 1
+                break
+
+        df_remove_duplication = self.df_raw.iloc[head_duplicate_last_idx:end_duplicate_last_idx]
+        return df_remove_duplication
--- psm_test.py
+++ psm_test.py
@@ -9,3 +9,7 @@ if __name__ == "__main__":
 
     CONSTS = psm.Constants(pitch_length=20e-3)
     measurement = psm.MeasurementDataDivide(filepath="raw_data/1216/RT_202112161123_FTP.txt")
+
+# %%
+    importlib.reload(psm)
+    circlepath = psm.CirclePathIntegration(measurement.raw_df_list[0])
