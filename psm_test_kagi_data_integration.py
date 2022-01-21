fba855e01825558180ba7a18d2a24438f0ec0f8e
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-01-21 16:49:31 +0900

    図の追加

--- psm_test_kagi_data_integration.py
+++ psm_test_kagi_data_integration.py
@@ -24,10 +24,9 @@ if __name__ == "__main__":
                                     integration_optimize_init=-5e4,
                                     height_optimize_init=[-3.5e4, 70, -3.5e4])
 
-# %%
-    fig1 = plt.figure(figsize=(12, 4))
+    fig1 = plt.figure(figsize=(12, 8))
 
-    gs1 = fig1.add_gridspec(2, 1)
+    gs1 = fig1.add_gridspec(4, 1)
     ax11 = fig1.add_subplot(gs1[0, 0])
     ax11.scatter(mes.df_float["theta"], mes.df_float["Out1"], s=1, label="s1")
     ax11.scatter(mes.df_float["theta"], mes.df_float["Out2"], s=1, label="s2")
@@ -38,20 +37,40 @@ if __name__ == "__main__":
     ax12.plot(itg.theta, itg.sag)
     ax12.plot(itg.theta, itg.sag_optimize_removing)
 
+    ax13 = fig1.add_subplot(gs1[2, 0])
+    ax13.plot(itg.theta, itg.height)
+    ax13.plot(itg.theta, itg.height_removing)
+    ax13.set_ylabel("height [nm]")
+
+    ax14 = fig1.add_subplot(gs1[3, 0])
+    ax14.plot(itg.theta, itg.height_removed)
+    ax14.set_ylabel("height_deviation [nm]")
+    ax14.set_xlabel("robot-arm scanning coordinate [mm]")
+
     fig2 = plt.figure(figsize=(12, 12))
     gs2 = fig2.add_gridspec(5, 1)
 
     ax21 = fig2.add_subplot(gs2[0, 0])
     ax21.plot(-itg.circumference, itg.sag)
     ax21.plot(-itg.circumference, itg.sag_optimize_removing)
+    ax21.set_ylabel("sag [nm]")
 
     ax22 = fig2.add_subplot(gs2[1, 0])
     ax22.plot(-itg.circumference, itg.sag_diff)
+    ax22.set_ylabel("sag deviation [nm]")
 
     ax23 = fig2.add_subplot(gs2[2, 0])
     ax23.plot(-itg.circumference, itg.tilt)
+    ax23.set_ylabel("gradient [nm / pitch]")
 
     ax24 = fig2.add_subplot(gs2[3, 0])
     ax24.plot(-itg.circumference, itg.height)
+    ax24.plot(-itg.circumference, itg.height_removing)
+    ax24.set_ylabel("height [nm]")
 
     ax25 = fig2.add_subplot(gs2[4, 0])
+    ax25.plot(-itg.circumference, itg.height_removed)
+    ax25.set_ylabel("height_deviation [nm]")
+    ax25.set_xlabel("robot-arm scanning coordinate [mm]")
+
+    fig2.tight_layout()
